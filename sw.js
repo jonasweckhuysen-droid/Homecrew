ee668cf9c4aec9c9d88b11a4a1dc69e06254df2a
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2025-11-30 23:26:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,26 @@
-const CACHE_NAME = "homecrew-cache-v1";
-const urlsToCache = [
-    "/Homecrew/index.html",
-    "/Homecrew/dashboard.html",
-    "/Homecrew/agenda.html",
-    "/Homecrew/taken.html",
-    "/Homecrew/style.css",
-    "/Homecrew/script.js",
-    "/Homecrew/manifest.json"
+const cacheName = "homecrew-v1";
+
+const assets = [
+  "./",
+  "./index.html",
+  "./dashboard.html",
+  "./agenda.html",
+  "./taken.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
