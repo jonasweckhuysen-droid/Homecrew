128d0fedb6811848f882e1bca1147083f9cceb8d
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2025-12-01 09:11:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,53 @@
-const cacheName = "homecrew-v1";
+const CACHE_NAME = "homecrew-v3";  // <-- elke update dit nummer verhogen
 
-const assets = [
-  "./",
-  "./index.html",
-  "./dashboard.html",
-  "./agenda.html",
-  "./taken.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const URLS_TO_CACHE = [
+  "/Homecrew/",
+  "/Homecrew/index.html",
+  "/Homecrew/dashboard.html",
+  "/Homecrew/taken.html",
+  "/Homecrew/agenda.html",
+  "/Homecrew/style.css",
+  "/Homecrew/script.js",
+  "/Homecrew/manifest.json",
+  "/Homecrew/icons/icon-192.png",
+  "/Homecrew/icons/icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+// Install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activate (verwijder oude caches)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch met update-strategie
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => caches.match(event.request))
   );
 });
